9c107896f50d74b592d40117646458aade1c14b5
ziyiliu02 <ziyi.liu02@gmail.com>
2023-11-17 13:41:28 +0800

    Initial Commit

---
+++ 10-swiftConcepts-bitrise/_0_swiftConcepts_bitriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_swiftConcepts_bitriseApp.swift
+//  10-swiftConcepts-bitrise
+//
+//  Created by Liu Ziyi on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct _0_swiftConcepts_bitriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
